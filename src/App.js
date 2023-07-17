0fc483a4544d2a7c2f4d2366807d3842ef37e8db
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2023-07-18 01:04:01 +0900

    update: firebase 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,88 @@
+import { useState, useEffect } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+import { auth } from "./firebase";
+
 function App() {
-  return <>메인으로 머지해볼까?</>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user?.email);
+    });
+  }, []);
+
+  return (
+    <>
+      {currentUser ? (
+        <>
+          <div>{currentUser}</div>
+          <button
+            onClick={async () => {
+              alert("로그아웃 할까?");
+              await signOut(auth);
+            }}
+          >
+            로그아웃
+          </button>
+        </>
+      ) : (
+        <>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <button
+            onClick={async () => {
+              try {
+                const userCredential = await createUserWithEmailAndPassword(
+                  auth,
+                  email,
+                  password
+                );
+              } catch (error) {
+                console.error(error);
+              }
+            }}
+          >
+            회원가입
+          </button>
+          <button
+            onClick={async (event) => {
+              event.preventDefault();
+              try {
+                const userCredential = await signInWithEmailAndPassword(
+                  auth,
+                  email,
+                  password
+                );
+              } catch (error) {
+                console.error(error);
+              }
+            }}
+          >
+            로그인
+          </button>
+        </>
+      )}
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import store from "./store";
-import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
+  <QueryClientProvider client={queryClient}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Provider>
+  </QueryClientProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
