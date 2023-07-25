2972537a156919e3b849f73a13f87d2d989a26dc
seonik <ikik.pd@gmail.com>
2023-07-25 11:44:57 +0900

    Docs. ReadMe, Router 폴더 위치 변경

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { FaHome } from "react-icons/fa";
+import {Link, useNavigate} from "react-router-dom";
+import {FaHome} from "react-icons/fa";
 
 export default function Header() {
   const navigate = useNavigate();
