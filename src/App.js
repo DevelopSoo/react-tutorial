81dd80d64ea48b7bbd088ca33a9b67bd7bb44067
leesoyul <tndus1303@gmail.com>
2023-07-25 15:53:11 +0900

    Feat / useState를 활용하여 임시 데이터를 만들고 메인페이지에 보여줌

--- src/App.js
+++ src/App.js
@@ -5,13 +5,36 @@ import Create from "./pages/Create";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Edit from "./pages/Edit";
+import uuid from "react-uuid";
+import { useState } from "react";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      id: uuid(),
+      title: "제목1",
+      content: "내용1",
+      author: "호빵",
+    },
+    {
+      id: uuid(),
+      title: "제목2",
+      content: "내용2",
+      author: "찐빵",
+    },
+    {
+      id: uuid(),
+      title: "제목3",
+      content: "내용3",
+      author: "식빵",
+    },
+  ]);
+
   return (
     // 페이지 이동에 사용되는 Route 태그를 위해선 Routes로 먼저 감싸야 한다.
     <Routes>
       {/* path="/"이기 때문에 '<주소>/'인 주소로 접속할 경우 Main 컴포넌트가 화면에 보여지게 된다.  */}
-      <Route path="/" element={<Main />} />
+      <Route path="/" element={<Main posts={posts} />} />
       <Route path="/detail/:id" element={<Detail />} />
       <Route path="/create" element={<Create />} />
       <Route path="/edit" element={<Edit />} />
