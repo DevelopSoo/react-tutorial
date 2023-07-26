5ebd131111682da770e7863ead03b2a2b3fbe0fe
seonik <ikik.pd@gmail.com>
2023-07-26 11:03:30 +0900

    feat. delete

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -6,7 +6,7 @@ import { useSelector } from 'react-redux';
 import Button from '../../components/button/Button';
 import * as S from './Detail.styled';
 
-export default function Detail() {
+export default function Detail({ todos, setTodos }) {
   //useParams를 사용해 URL에서 id 값을 추출합니다.
   const { id } = useParams();
   //Redux Toolkit을 사용해 todos를 불러옵니다.
@@ -17,12 +17,17 @@ export default function Detail() {
   // todos id와 params id 값을 비교해서 일치하는 첫번째 항목을 가져옵니다.
   const filteredTodo = todos.find((todo) => todo.id === id);
 
-  const deleteHandler = () => {
-    if (!window.confirm('삭제할까')) {
-      return;
-    }
+  const deleteHandler = (itemId) => {
+    alert('삭제할까?');
 
-    navigate('/');
+    if (!window.confirm) {
+      return;
+    } else {
+      // todos의 데이터에서 클릭한 요소를 찾아 삭제하는 코드
+      const newTodos = todos.filter((todo) => todo.id !== itemId);
+      setTodos(newTodos);
+      navigate('/');
+    }
   };
 
   if (!filteredTodo) {
@@ -54,7 +59,7 @@ export default function Detail() {
           >
             수정
           </Button>
-          <Button variant="solid" color="red" onClick={deleteHandler}>
+          <Button variant="solid" color="red" onClick={deleteHandler(filteredTodo.id)}>
             삭제
           </Button>
         </div>
