c4012e264ee93945b9f3042393eeccf6758ec74d
leesoyul <tndus1303@gmail.com>
2023-07-30 04:15:32 +0900

    Feat / add 404 page

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import Create from "./pages/Create";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Edit from "./pages/Edit";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    // 페이지 이동에 사용되는 Route 태그를 위해선 Routes로 먼저 감싸야 한다.
     <Routes>
-      {/* path="/"이기 때문에 '<주소>/'인 주소로 접속할 경우 Main 컴포넌트가 화면에 보여지게 된다.  */}
       <Route path="/" element={<Main />} />
       <Route path="/detail/:id" element={<Detail />} />
       <Route path="/create" element={<Create />} />
       <Route path="/edit" element={<Edit />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
