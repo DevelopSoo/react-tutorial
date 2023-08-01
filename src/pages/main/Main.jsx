ef3a455b7f78236391ab4b705dadeefcc84b2381
seonik <ikik.pd@gmail.com>
2023-08-01 10:12:15 +0900

    feat. redux toolkit 완료

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,33 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../../common/Header';
 import Container from '../../common/Container';
 import * as S from './Main.styled';
 import Button from '../../components/button/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { remove } from '../../redux/modules/todosSlice';
 
-export default function Main({ todos, setTodos }) {
-  /*
-  게시물 데이터엔 다음과 같은 데이터가 존재해야 합니다.
-    - id - 고유한 아이디여야 합니다
-    - title - 제목입니다.
-    - content - 내용입니다.
-    - author - 작성자입니다.
-  */
+export default function Main() {
+  // redux Todos를 연결합니다
+  const reduxTodos = useSelector((state) => state.todos);
 
-  const deleteHandler = (item) => {
-    alert('삭제할까?');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    // 구글검색
-    if (!window.confirm) {
-      return;
+  const [verify, setVerify] = useState(null);
+
+  const verifyUser = () => {
+    if (reduxTodos.author === '김선익') {
+      return setVerify(true);
+    } else {
+      return setVerify(false);
     }
-
-    // todos의 데이터에서 클릭한 요소를 찾아 삭제하는 코드
-    const newTodos = todos.filter((todo) => todo.id !== item.id);
-    setTodos(newTodos);
   };
 
-  const navigate = useNavigate();
+  useEffect(() => {
+    verifyUser();
+  }, []);
 
   return (
     <>
@@ -45,9 +44,9 @@ export default function Main({ todos, setTodos }) {
           </Button>
         </S.ButtonLayout>
 
-        {/* useState + map 임시 데이터 3개 보여주기 */}
+        {/* reudTodos + map 임시 데이터 3개 보여주기 */}
         {/* initialState로 정의한 3개의 임시데이터를 todos의 초기값으로 정의하고 map을 사용합니다. */}
-        {todos.map((item) => {
+        {reduxTodos.map((item) => {
           //todos에 들어있는 데이터를 사용하기 위해 item이라는 파라미터를 사용합니다.
           return (
             <>
@@ -67,26 +66,32 @@ export default function Main({ todos, setTodos }) {
                 <S.TodoItemAuth>
                   {/* item.author에 접근해 작성자를 출력합니다. */}
                   <S.Author>{item.author}</S.Author>
-                  <div>
-                    <Button
-                      variant="solid"
-                      color="orange"
-                      onClick={() => {
-                        navigate(`/edit/${item.id}`);
-                      }}
-                    >
-                      수정
-                    </Button>
-                    <Button
-                      variant="solid"
-                      color="red"
-                      onClick={() => {
-                        deleteHandler(item);
-                      }}
-                    >
-                      삭제
-                    </Button>
-                  </div>
+                  {verify ? (
+                    <div>
+                      <Button
+                        variant="solid"
+                        color="orange"
+                        onClick={(e) => {
+                          navigate(`/edit/${item.id}`);
+                          // id값을 url로 넣어주면 보안에 위험이 있다.
+                          // 수정은 권한이 있는 사람만 해야하기 때문에 URL형태로 관리하면 안됩니다.
+                          // navigate('/edit', { state: { post } });
+                        }}
+                      >
+                        수정
+                      </Button>
+
+                      <Button
+                        variant="solid"
+                        color="red"
+                        onClick={() => {
+                          dispatch(remove(item.id));
+                        }}
+                      >
+                        삭제
+                      </Button>
+                    </div>
+                  ) : null}
                 </S.TodoItemAuth>
               </S.TodoItem>
             </>
